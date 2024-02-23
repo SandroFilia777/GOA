adb8b4f87cfdb8a34fdfdfa9618ddb6b8be466b2
SandroFilia777 <150830093+SandroFilia777@users.noreply.github.com>
2024-02-23 21:53:01 +0400

    commit

---
+++ Day 07/classwork/classwork.py
@@ -0,0 +1,5 @@
+name = input("გთხოვთ, შეიყვანოთ თქვენი სახელი:")
+lastname = input("გთხოვთ, შეიყვანოთ თქვენი გვარი:")
+age = input("გთხოვთ, შეიყვანოთ თქვენი ასაკი:")
+
+print("puts your  imputs here:"name, lastname, age)
---
+++ Day 07/classwork/classwork1.py
@@ -0,0 +1,6 @@
+num1= input('Please enter your first number')
+num2= input('Please enter your second number')
+
+print(num1 < num2)
+print(num1 > num2)
+print(num1 == num2)
