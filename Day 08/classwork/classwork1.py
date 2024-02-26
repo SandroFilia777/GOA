04a8689ff98d46225eee21fcc458b7590fc532d9
SandroFilia777 <150830093+SandroFilia777@users.noreply.github.com>
2024-02-26 23:10:43 +0400

    commit

---
+++ Day 08/classwork/classwork1.py
@@ -0,0 +1,4 @@
+name = "sandro"
+NAME = "luka"
+Name = "gio"
+print(name, NAME, Name)
---
+++ Day 08/homework/homework.py
@@ -0,0 +1,5 @@
+decision=input("which one is better, real-madrid or FC Barcelona? - ")
+if decision == "FC Barcelona" or decision =="fc barcelona" or decision =="barca" :
+    print("You are 100% right")
+else:
+    print("You are wrong")
