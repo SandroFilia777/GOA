9fd2c8d178df56d2fb76584f59e3557c68c2f870
SandroFilia777 <150830093+SandroFilia777@users.noreply.github.com>
2024-03-03 01:36:26 +0400

    commit

---
+++ python/Day 08/homework/homework1.py
@@ -0,0 +1,75 @@
+user_input=int(input("please enter number"))
+
+if user_input==-32:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
+if user_input==34:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
+if user_input==43:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
+if user_input==-93:
+    print("you won!")
+else:
+    print("you lose!")
+
+
+
+user_input=int(input("please enter number"))
+
+if user_input==2:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
+if user_input==434:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
+if user_input==65:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
+if user_input==77:
+    print("you won!")
+else:
+    print("you lose!")
+
+
+user_input=int(input("please enter number"))
+
+if user_input==21:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
+if user_input==-554:
+    print("you won!")
+else:
+    print("you lose!")
+
+user_input=int(input("please enter number"))
+
