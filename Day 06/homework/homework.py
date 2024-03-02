21571609aadc505cd30b57f4dc443da9fef26243
SandroFilia777 <150830093+SandroFilia777@users.noreply.github.com>
2024-03-02 23:00:13 +0400

    commit

--- Day 06/homework/homework.py
+++ Day 06/homework/homework.py
@@ -1,21 +1,20 @@
 num1=int(input("please enter number1;"))
 num2=int(input("please enter number2;"))
+
+print(num1+num2)
+
 num3=int(input("please enter number3;"))
-print(num1+num2+num3)
+num4=int(input("please enter number4;"))
 
+print(num3-num4)
 
-num4=int(input("please enter number10;"))
-num5=int(input("please enter number11;"))
-num6=int(input("please enter number12;"))
-print(num4/num5/num6)
+num5=int(input("please enter number5;"))
+num6=int(input("please enter number6;"))
+
+print(num5/num6)
 
 num7=int(input("please enter number7;"))
 num8=int(input("please enter number8;"))
-num9=int(input("please enter number9;"))
-print(num7*num8*num9)
 
+print(num7*num8)
 
-num10=int(input("please enter number4;"))
-num11=int(input("please enter number5;"))
-num12=int(input("please enter number6;"))
-print(num10-num11-num12)
---
+++ Day 09/classwork/classwork.py
@@ -0,0 +1,7 @@
+print("sandro")
+
+print("luka")
+
+print("saba")
+
+print("dato")
