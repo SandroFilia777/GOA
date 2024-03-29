5228f67c5d3dbe555d15c43e534a6e353bbe4c0e
SandroFilia777 <sandro.filia2010@gmail.com>
2024-03-29 17:39:01 +0400

    commit

---
+++ Day 16/classwork/classwork.py
@@ -0,0 +1,19 @@
+#1
+
+list1=["BMW","TOYOTA","FERRARI","ASTON MARTIN","KOENIGSEGG","NISSAN"]
+print(list1[0])
+print(list1[2])
+print(list1[4])
+
+# 2
+list2=["Sandro", "Filia"]
+print(list2[0])
+print(list2[1])
+
+
+# 3
+age=int(input("Please enter your age: "))
+email=(input("Please enter your email: "))
+
+list3=[age , email]
+print(list3)
---
+++ Day 16/homework/homework.py
@@ -0,0 +1,29 @@
+list1=["nini","namo", "giorgi","nika", "dato"]
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3])
+print(list1[4])
+
+
+
+list2=[1,2,3,4,5,6,7,8,9,10]
+print(list2[0])
+print(list2[9])
+
+
+number = [1 , 2 , 3, 4,5,6,7,8,9,10]
+print (number[0] , number[1] , number[2] , number[3] , number[4] , number[5] , number[6] , number[7] , number[8] , number[9] )
+
+
+name="sandro"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+
+
+list4=[input("please enter your name;"), input("please enter your surname;"),int(input("please enter your age;")),input("please enter your mail;"),input("please enter your address;")]
+print(list4[0], list4[1], list4[2], list4[3], list4[4] )
