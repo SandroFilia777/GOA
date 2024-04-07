461e08d04119f3b25d7923790b8a895efa1908c9
SandroFilia777 <sandro.filia2010@gmail.com>
2024-04-08 00:27:23 +0400

    commit

---
+++ Day 19/homework/homework.py
@@ -0,0 +1,15 @@
+list_of_names=["sandro", "dato", "luka", "nika","mate"]
+input1=int(input("Please enter number from 0 to 4: "))
+if input1 == 0:
+    print("Sandro")
+elif input1 == 1:
+    print("dato")
+elif input1 == 2:
+    print("luka")
+elif input1 == 3:
+    print("nika")
+elif input1 == 4:
+    print("mate")
+else:
+    print("something went wrong. try again...")
+    
