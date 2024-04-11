552752856b675de0379b3b5c7b751c87d7e9cd8b
SandroFilia777 <sandro.filia2010@gmail.com>
2024-04-12 01:01:44 +0400

    commit

--- Day 20/homework/homework.py
+++ Day 20/homework/homework.py
@@ -11,24 +11,39 @@
 #2
 
 
+def find_duplicates():
+    person_input = int(input("Tell me how many times do you want to enter numbers: "))
+    list1 = []
+    for i in range(person_input):
+        new_num = int(input("Enter number: "))
+        list1.append(new_num)
+    list2 = []
+    for num in list1:
+        if list1.count(num) >= 2 and num not in list2:
+            list2.append(num)
+    if len(list2) == 0:
+        print("List is empty")
+    else:
+        print(list2)
+find_duplicates()
 
 
-#3
-def add_first_letter(words):
-    new_word = ""
-    for word in words:
-        new_word += word[0]
-    return new_word
+# #3
+# def add_first_letter(words):
+#     new_word = ""
+#     for word in words:
+#         new_word += word[0]
+#     return new_word
 
-words = input("შეიყვანეთ ხუთი სიტყვა: ").split()
-result = add_first_letter(words)
-print(result)
+# words = input("შეიყვანეთ ხუთი სიტყვა: ").split()
+# result = add_first_letter(words)
+# print(result)
 
 
 
-#4
+# #4
 
-first = list(range(10, 21))
-second = list(range(30, 51, 5))
-combined = first + second
-print(combined)
+# first = list(range(10, 21))
+# second = list(range(30, 51, 5))
+# combined = first + second
+# print(combined)
---
+++ HACKATON 02/codes.py
@@ -0,0 +1,126 @@
+ #1
+# a=1
+# b=2
+# print(a+b)
+
+# #2
+# a=2
+# b=3
+# print(a**b)
+
+
+# 3
+# list = [1,2,5,3,4]
+# print(max(list) - min(list))
+
+# 4
+# a = 6
+# b = 10
+
+# if a>b:
+#     i = a
+# elif b>a:
+#     i = b
+
+
+# while i % a != 0 or i % b != 0:
+#     i+=1
+# print(i)
+
+
+# 5
+# name = "goal oriented academy"
+# modify = name.replace(" ", "")
+
+# print(modify)
+
+# 6 
+# name = "goal oriented academy"
+# print(name.count("g"))
+
+# 7
+# num = [1, 4, 5, 20, 3, 2] 
+# a = max(num)
+# num.remove(max(num))
+# b = max(num)
+
+# print(a + b)
+
+# 8 
+# list1 = [5, 4, 12, 20, 13, 144, 23]
+# list2 = [13, 20, 23, 144]
+# for i in list1:
+#     for x in list2:
+#         if x >= i:
+#             print(i)
+
+
+# 9
+# num = [1,2,5,6,7]
+# for i in num:
+#     if 12 % i == 0:
+#         print(i)
+
+
+# 10
+# num = int(input("please enter a number: "))
+# my_arr = []
+# for i in range(2, num + 1):
+#     if num % i == 0:
+#         my_arr.append(i)
+# if len(my_arr) > 2:
+#     print("Rtulia")
+# else:
+#     print("Martivia")
+
+# 11
+# list1 = [1,9,3,3,4]
+# list2 = [4,3,5]
+# list3 = []
+# for i in list1:
+#     for x in list2:
+#         if x == i:
+#             print(i)
+
+
+#12
+# name = "goal oriented"
+# name=name.split(" ")
+# new_list = []
+# for word in name:
+#     new_word=""
+#     for i in word:
+#         if i != word[0]:
+#             new_word+=i
+#     new_list.append(new_word)
+# print(new_list)
+
+#13
+# DNMset = {"M","A","T","E"}
+# set1 = {"M","A","T","T","A","T","E","A"}
+# set2 = {"M", "T", "A", "T", "A", "E", "E", "E"}
+
+# i = DNMset.intersection(set1 and set2)
+# print(i)
+# if i == DNMset:
+#     print("no")
+# else:
+#     print("yes")
+
+# set3 = {"M","A","T","E","E","E","E","E"}
+# set4 = {"B","A","T","E","E","E","E","E"}
+# i = DNMset.intersection(set3 and set4)
+# print(i)
+# if i == DNMset:
+#     print("no")
+# else:
+#     print("yes")
+
+
+
+# 14
+# name = "goaloriented"
+# print(name[::-1])
+
+# 15    
+
