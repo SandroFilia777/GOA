f77142917898670a5fc50c4dff44e244965a7a29
SandroFilia777 <sandro.filia2010@gmail.com>
2024-05-24 19:22:10 +0400

    commit

---
+++ Day 32/classwork/classwork.py
@@ -0,0 +1,4 @@
+def manual_index (numbers_list, search_value):
+    if search_value not in numbers_list:
+        return -1
+    
---
+++ Day 32/homework/.py
@@ -0,0 +1,45 @@
+#დავალება_2
+# def manual_index(numbers_list , search_value):
+#     for i in range(len(numbers_list)):
+#         if numbers_list[i] == search_value:
+#             return i
+   
+
+# result = manual_index([1,2,3,4,5,5,67,8,9], 3)
+# print(result)
+
+#დავალება_3
+# def  manual_max(number_list):
+#     max_value=number_list[0]
+
+#     for i in number_list[1:]:
+#         if i > max_value:
+#             max_value = i
+#     return i
+
+# result =manual_max([1,2,3,4,557,700])
+# print(result)
+
+
+# def  manual_max(number_list):
+#     min_value=number_list[0]
+
+#     for i in number_list[1:]:
+#         if i < min_value:
+#             min_value = i
+#     return i
+
+# result =manual_max([2,3,4,557,700,1])
+
+# print(result)
+
+#დავალება_4
+# def manual_pop(lst,indx):
+#     new_list = []
+#     for i in range(len(lst)):
+#         if i != indx:
+#             new_list.append(lst[i])
+#     return new_list
+
+# result = manual_pop([1,2,3,4,5,5,77], 1)
+# print(result)
